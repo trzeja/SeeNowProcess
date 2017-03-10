127244dd942da8b24279cb7399b04fbced8db6ff
trzej_000 <trzej_000@MTRZEJA01>
2017-03-10 09:01:30 +0100

    Add project files.

---
+++ SeeNowProcess/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeeNowProcess.MvcApplication" Language="C#" %>
---
+++ SeeNowProcess/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeeNowProcess.Startup))]
+namespace SeeNowProcess
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
