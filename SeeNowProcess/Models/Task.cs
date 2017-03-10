c14a426853f43e14a4a53bce73f7795bd40f8782
iko <trzeja.mikolaj@gmail.com>
2017-03-10 19:58:26 +0100

    Dodanie klasy Task  do modelu

---
+++ SeeNowProcess/Models/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeeNowProcess.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public Importance Importance { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public State CurrentState { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime CompletionDate { get; set; }        
+        public decimal Progress { get; set; }
+    }
+
+    public enum State
+    {
+        Created,
+        InProgress,
+        Finished
+    }
+
+    public enum Importance
+    {
+        None,
+        Trivial,
+        Regular,
+        Important,
+        Critical
+    };
+}
