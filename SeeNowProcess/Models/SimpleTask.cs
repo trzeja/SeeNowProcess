1125175782f875ef73aa640b3a744ad8df63271c
iko <trzeja.mikolaj@gmail.com>
2017-03-12 12:18:03 +0100

    Dodano DataAnnotations aby nazwy kolumn były bardziej przyjazne użytkownikowi

--- SeeNowProcess/Models/SimpleTask.cs
+++ SeeNowProcess/Models/SimpleTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -12,8 +13,18 @@ namespace SeeNowProcess.Models
         public Importance Importance { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        [Display(Name = "Current State")]        
         public State CurrentState { get; set; }
+
+        [Display(Name = "Creation Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreationDate { get; set; }
+
+        [Display(Name = "Completion Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CompletionDate { get; set; }        
         public decimal Progress { get; set; }
     }
