182ba807726ab404aaddb126cd94649f83bf960b
iko <trzeja.mikolaj@gmail.com>
2017-03-16 19:15:03 +0100

    dodanie migracji i metody seed z przykladowymi danymi

---
+++ SeeNowProcess/Migrations/Configuration.cs
@@ -0,0 +1,56 @@
+namespace SeeNowProcess.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SeeNowProcess.Models.SimpleTaskDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SeeNowProcess.Models.SimpleTaskDBContext context)
+        {
+            context.SimpleTasks.AddOrUpdate(i => i.Title,
+                new SimpleTask
+                {
+                    Title = "title1",
+                    CreationDate = DateTime.Parse("1989-1-11"),
+                    CompletionDate = DateTime.Parse("1989-1-11"),
+                    Importance = Importance.Critical,
+                    CurrentState = State.Finished,
+                    Description = "desc1",
+                    Progress = 80
+                },
+
+                new SimpleTask
+                {
+                    Title = "title2",
+                    CreationDate = DateTime.Parse("1989-1-11"),
+                    CompletionDate = DateTime.Parse("1989-1-11"),
+                    Importance = Importance.Regular,
+                    CurrentState = State.InProgress,
+                    Description = "desc2",
+                    Progress = 50
+                },
+
+                new SimpleTask
+                {
+                    Title = "title3",
+                    CreationDate = DateTime.Parse("1989-1-11"),
+                    CompletionDate = DateTime.Parse("1989-1-11"),
+                    Importance = Importance.Trivial,
+                    CurrentState = State.Created,
+                    Description = "desc3",
+                    Progress = 20
+                }
+           );
+
+        }
+
+    }
+}
