e2a89dbf73b5eda1eecf46c3454b3fff6f2b93b1
iko <trzeja.mikolaj@gmail.com>
2017-04-01 11:43:07 +0200

    dodanie klasy UserStory

---
+++ SeeNowProcess/Models/UserStory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SeeNowProcess.Models
+{
+    public class UserStory
+    {
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Size { get; set; }
+        public string Unit { get; set; }
+        public string Notes { get; set; }
+        //public User Owner {get;set;}
+        //IEnumerable<Team> Teams { get; set; } //to
+        //public Team Team { get; set; }        //lub to
+        public string Criteria { get; set; }
+        //public Project Project { get; set; }
+
+    }
+}
