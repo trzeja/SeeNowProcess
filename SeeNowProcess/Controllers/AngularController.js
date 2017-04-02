60ebd8e48c3a53c072e3af5fdb9027188fdee4c9
Roksana Paluch (rokspal895) <rokspal895@student.polsl.pl>
2017-04-03 01:01:35 +0200

    Rozdzielenie kontrolera Angulara (nie wiem czy dalam w dobre miejsce)

---
+++ SeeNowProcess/Controllers/AngularController.js
@@ -0,0 +1,42 @@
+﻿var taskApp = angular.module("taskForm", []);
+taskApp.controller("taskCtrl", function ($scope) {
+    $scope.tasks =
+        [
+            {
+                title: "Aaa", description: "Bbb",
+                status: "open", importance: "trivial",
+                estimated_time: "2 weeks", parent: "s"
+            }
+        ];
+    /* Roksana:
+    1. Nie dodaję na razie "Choose an option" które dodała Ania, zostaje puste pole które po wyborze opcji
+    znika z listy wyboru. Nie wiem na razie czy to dobry pomysł, zależy czy to będzie obowiązkowe pole (?)
+    2. Chyba niepotrzebnie dodałam to jako obiekty, ale proszę o weryfikację */
+    $scope.status_options =
+        [
+            { value: "open", description: "Open" },
+            { value: "suspended", description: "Suspended" },
+            { value: "closed", description: "Closed" }
+        ];
+    $scope.importance_options =
+        [
+             { value: "none", description: "None" },
+             { value: "trivial", description: "Trivial" },
+             { value: "regular", description: "Regular" },
+             { value: "important", description: "Important" },
+             { value: "critical", description: "Critical" }
+        ];
+    $scope.parent_options =
+        [
+             { value: "s", description: "Small (S)" },
+             { value: "m", description: "Medium (M)" },
+             { value: "l", description: "Large (L)" },
+             { value: "xl", description: "Extra large (XL)" },
+        ];
+    $scope.addTask = function () {
+        $scope.tasks.push({
+            title: $scope.title, description: $scope.description, status: $scope.status.value,
+            importance: $scope.importance.value, estimated_time: $scope.estimated_time, parent: $scope.parent.value
+        });
+    }
+});
