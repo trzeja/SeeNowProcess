2393f243333e822f52a816c4c4c69a579ac4e6da
iko <trzeja.mikolaj@gmail.com>
2017-04-23 09:55:53 +0200

    Poprawki struktury w projekcie testów

---
+++ SeeNowProcess.Tests/DAL/DbSets/TestAssignmentDbSet.cs
@@ -0,0 +1,18 @@
+﻿using SeeNowProcess.Models;
+using SeeNowProcess.Tests.DAL.DbSets.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeNowProcess.Tests.DAL.DbSets
+{
+    class TestAssignmentDbSet:TestDbSet<Assignment>
+    {
+        public override Assignment Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(assignment => assignment.AssignmentID == (int)keyValues.Single());
+        }
+    }
+}
