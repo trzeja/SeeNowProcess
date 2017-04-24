8a418289575729cdb795f0b7200e52429539c3dc
Anna Kocztorz <annakoc476@student.polsl.pl>
2017-04-24 19:01:46 +0200

    Wstępne dodanie widoku do rejestracji (dopracuję go jeszcze)

--- SeeNowProcess/Scripts/angularRegister.js
+++ SeeNowProcess/Scripts/angularRegister.js
@@ -1,5 +1,5 @@
 ﻿var registerApp = angular.module("registerForm", []);
-registerApp.controller("registerCtrl", function ($scope) {
+registerApp.controller("registerCtrl",  [ '$scope', '$http', function ($scope, $http) {
     $scope.users =
         [
             {
@@ -8,10 +8,30 @@ registerApp.controller("registerCtrl", function ($scope) {
                 phone: ""
             }
         ];
-
+    $scope.message = '';
     $scope.registerUser = function () {
-        $scope.users.push({
-            login: $scope.login, password: $scope.password, name: $scope.name, email: $scope.email, phone: $scope.phone
-        });
+        if ($scope.password !== $scope.confirmPassword)
+            $scope.message = "Incorrect password";
+        else {
+            $http({
+                method: "POST",
+                url: "/Account/RegisterAction",
+                data: $.param({ 'password': $scope.password, 'login': $scope.login, 'name': $scope.name, 'email': email, 'phone': phone}),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+
+            }).then(function mySucces(response) {
+                if (response.data === "Success")
+                    window.location.href = "/People/Index";
+                else
+                    $scope.message = "Error - incorrect data!";
+            }, function myError(response) {
+                $scope.message = "Error";
+            })
+
+            $scope.users.push({
+                login: $scope.login, password: $scope.password, name: $scope.name, email: $scope.email, phone: $scope.phone
+            });
+        }
+        
     }
-});
+}]);
