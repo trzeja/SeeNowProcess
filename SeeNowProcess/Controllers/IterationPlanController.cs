9a216e320fe691e3f3f52d0e0f0e881119d89fdb
Krzysztof Krawczyk <krawczykk@hotmail.com>
2017-05-07 15:16:39 +0200

    Kontroler do planu iteracji, v1.0

--- SeeNowProcess/Controllers/IterationPlanController.cs
+++ SeeNowProcess/Controllers/IterationPlanController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SeeNowProcess.Controllers;
+using SeeNowProcess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +8,7 @@ using System.Web.Mvc;
 
 namespace Projekt_programistyczny_pierwsze_kroki.Controllers
 {
-    public class IterationPlanController : Controller
+    public class IterationPlanController : DIContextBaseController
     {
         // GET: IterationPlan
         public ActionResult Index()
@@ -17,24 +19,63 @@ namespace Projekt_programistyczny_pierwsze_kroki.Controllers
         [HttpPost]
         public ActionResult AddingIteration()
         {
-            return View();  /* tutaj dodawanie iteracji do bazy poprosze
-                                
-                            */
+            using (db)
+            {
+                // chyba teraz nie rozrozniamy projektu
+                Project project = db.Projects.FirstOrDefault();
+                Iteration iteration = new Iteration() {Description="", Name="Unnamed", Project=project};
+                //zabezpieczenie przed duplikacją nazw "Unnamed"
+                List<Iteration> unnameds;
+                unnameds = db.Iterations
+                             .Where(i => i.Project == project && i.Name.StartsWith("Unnamed"))
+                             .OrderBy(i => i.Name)
+                             .ToList();
+                int number = 0;
+                if (unnameds.Exists(i => i.Name.Equals(iteration.Name)))
+                {
+                    for (number = 1; unnameds.Exists(i => i.Name.Equals(iteration.Name + " (" + number + ")")); ++number) ;
+                    iteration.Name += " (" + number + ")";
+                }
+                db.Iterations.Add(iteration);
+                return new JsonResult { Data = "Success", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
         }
 
         [HttpGet]
         public ActionResult GetNumber()
         {
-            return View();  /*tutaj prosze o zwrocenie listy obiektow skladajacych sie z: 
+            using (db)
+            {
+
+                // chyba teraz nie rozrozniamy projektu
+                Project project = db.Projects.FirstOrDefault();
+                var iterations = db.Iterations
+                    .Where(i => i.Project == project)
+                    .Select(i => new
+                    {
+                        name = i.Name,
+                        duration = (i.StartDate == null ? "?" : i.StartDate.ToString()) + " - " + (i.EndDate == null ? "?" : i.EndDate.ToString())
+                    });
+                return Json(iterations, JsonRequestBehavior.AllowGet);
+
+            }
+                            /*tutaj prosze o zwrocenie listy obiektow skladajacych sie z: 
                                 {name: "nazwa iteracji", duration: "czas trwania(od kiedy do kiedy)"} 
                             */
         }
 
         [HttpPost]
-        public ActionResult GetIteration()
+        public ActionResult GetIteration(int id)
         {
-            //Request.Form["name"];
-            return View();  /* tutaj zwracamy taski z konkretnej iteracji (cala ich liste) - iteracja podana jako parametr (id) - 
+            using (db)
+            {
+                return Json(
+                    db.Problems
+                        .Where(p => p.Box.Iteration.IterationId == id)
+                    , JsonRequestBehavior.AllowGet);
+                    
+            }
+                /* tutaj zwracamy taski z konkretnej iteracji (cala ich liste) - iteracja podana jako parametr (id) - 
                             z wszystkich boxow same taski poprosze bo inaczej to sie widok zakopie totalnie...*/
         }
 
