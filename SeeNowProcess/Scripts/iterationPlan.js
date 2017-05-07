f5cbe5f267f3b2edb04cd00ecf01cf403cb01224
Anna Kocztorz <annakoc476@student.polsl.pl>
2017-05-07 08:39:48 +0200

    Iteration Plan - na 150% nie działa, ale wrzucam, żeby backend mógł się wykazać w kontrolerze ;)

---
+++ SeeNowProcess/Scripts/iterationPlan.js
@@ -0,0 +1,39 @@
+﻿var iterationApp = angular.module("iteration", []);
+iterationApp.controller("iterationCtrl", ['dndLists','$scope', '$http', function ($scope, $http) {
+    $scope.message = '';
+    $scope.iterations = [];
+    $scope.lists = [];
+    $scope.selected = null;
+    $scope.getIterations = function () {
+            $http({
+                method: "GET",
+                url: "/IterationPlan/GetNumber",
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+
+            }).then(function mySucces(response) {
+                iterations.push = response.data;
+            }, function myError(response) {
+                $scope.message = "Error";
+            })
+    }
+
+    $scope.getIterationsData = function () {
+        for ( var i=0; i<iterations.length();++i){
+            $http({
+                method: "POST",
+                url: "/IterationPlan/GetIteration",
+                data: $.param({"name": iterations.name}),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+
+            }).then(function mySucces(response) {
+                lists.push = response.data;
+            }, function myError(response) {
+                $scope.message = "Error";
+            })
+        }
+        
+        
+   }
+        
+
+}]);
