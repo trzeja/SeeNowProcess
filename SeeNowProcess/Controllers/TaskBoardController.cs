c76e6a09fde7ff7be8f37a80b99ed732b61b746d
Roksana Paluch <rokspal895@student.polsl.pl>
2017-05-09 17:12:36 +0200

    TaskBoard - tabelki

--- SeeNowProcess/Controllers/TaskBoardController.cs
+++ SeeNowProcess/Controllers/TaskBoardController.cs
@@ -4,14 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Projekt_programistyczny_pierwsze_kroki.Controllers
+namespace SeeNowProcess.Controllers
 {
-    public class TaskBoardController : Controller
+    public class TaskBoardController : DIContextBaseController
     {
         // GET: TaskBoard
-        public ActionResult Index()
+        public ActionResult TaskBoardIndex()
         {
             return View();
+        }
+
+        public ActionResult GetBoxes()
+        {
+            using (db)
+            {
+                var resultJ = db.Boxes.Select(a => new
+                {
+                    BoxOrder = a.Order,
+                    Name = a.Name
+                });
+                return new JsonResult { Data = resultJ.ToList(), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        public ActionResult GetUserStories()
+        {
+            using (db)
+            {
+                var resultJ = db.UserStories.Select(a => new
+                {
+                    UserSID = a.UserStoryID,
+                    Title = a.Title
+                });
+                return new JsonResult { Data = resultJ.ToList(), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
         }
     }
 }
--- SeeNowProcess/Scripts/taskBoard.js
+++ SeeNowProcess/Scripts/taskBoard.js
@@ -1,126 +1,125 @@
-﻿var taskBoardApp = angular.module("taskBoardPart", ['dndLists']);
-taskBoardApp.controller("taskBoardCtrl") , ['$scope', '$http', function ($scope, $http) {
-
-}]);
+﻿angular.module("taskBoardPart", ['dndLists']).controller("taskBoardCtrl", function ($scope, $http) {
+    $scope.message = "DZIALAAA";
+});
 //kopia IterationPlan do przerobienia
 
 
 
-var iterationApp = angular.module("iterationPart", ['dndLists']);
-iterationApp.controller("iterationCtrl", ['$scope', '$http', function ($scope, $http) {
-    $scope.message = 'Cos';
-    $scope.iterations = [];
-    $scope.lists = [];
-    $scope.selected = null;
-    /*  $scope.addIteration = function () {
-          $http({
-              method: "POST",
-              url: "/IterationPlan/AddingIteration",
-              data: $.param({ "name": iterations.Name }),
-              headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+//var iterationApp = angular.module("iterationPart", ['dndLists']);
+//iterationApp.controller("iterationCtrl", ['$scope', '$http', function ($scope, $http) {
+    
+//    $scope.iterations = [];
+//    $scope.lists = [];
+//    $scope.selected = null;
+//    /*  $scope.addIteration = function () {
+//          $http({
+//              method: "POST",
+//              url: "/IterationPlan/AddingIteration",
+//              data: $.param({ "name": iterations.Name }),
+//              headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
   
-          }).then(function mySucces(response) {
-              lists.push = response.data;
-          }, function myError(response) {
-              $scope.message = "Error";
-          })
-      }*/
-    /* $scope.iterations.push({ "Name": "Backlog", "StartDate": "dhefuieff", "EndDate": "jeoefof;rg" });
-     $scope.iterations.push({ "Name": "Backlog 2", "StartDate": "dhefuiesgfhff", "EndDate": "jeoeffhgfhfof;rg" });
-     $scope.iterations.push({ "Name": "Backlog 2", "StartDate": "dhefuiesgfhff", "EndDate": "jeoeffhgfhfof;rg" });
-     $scope.iterations.push({ "Name": "Backlog 2", "StartDate": "dhefuiesgfhff", "EndDate": "jeoeffhgfhfof;rg" });
-     for (var i = 0; i < $scope.iterations.length; ++i) {
-         var lista_pomocnicza = [];
-         lista_pomocnicza.push({ Title: "Sierotka Marysia", Description: "Whatever" });
-         lista_pomocnicza.push({ Title: "Sierotka Marysia2", Description: "Whatever Happens" });
-         $scope.lists.push(lista_pomocnicza);
-        // $scope.lists.i.push({ Title: "Sierotka Marysia", Description: "Whatever" });
-     }*/
-    $http({
-        method: "GET",
-        url: "/IterationPlan/GetNumber",
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
-    }).then(function mySucces(response) {
-        $scope.message = response.data;
-        $scope.iterations = response.data;
-        for (var i = 0; i < $scope.iterations.length; ++i) {
-            var id = $scope.iterations[i].id;
-            $http({
-                method: "POST",
-                url: "/IterationPlan/GetIteration",
-                data: $.param({ "id": $scope.iterations[i].id }),
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+//          }).then(function mySucces(response) {
+//              lists.push = response.data;
+//          }, function myError(response) {
+//              $scope.message = "Error";
+//          })
+//      }*/
+//    /* $scope.iterations.push({ "Name": "Backlog", "StartDate": "dhefuieff", "EndDate": "jeoefof;rg" });
+//     $scope.iterations.push({ "Name": "Backlog 2", "StartDate": "dhefuiesgfhff", "EndDate": "jeoeffhgfhfof;rg" });
+//     $scope.iterations.push({ "Name": "Backlog 2", "StartDate": "dhefuiesgfhff", "EndDate": "jeoeffhgfhfof;rg" });
+//     $scope.iterations.push({ "Name": "Backlog 2", "StartDate": "dhefuiesgfhff", "EndDate": "jeoeffhgfhfof;rg" });
+//     for (var i = 0; i < $scope.iterations.length; ++i) {
+//         var lista_pomocnicza = [];
+//         lista_pomocnicza.push({ Title: "Sierotka Marysia", Description: "Whatever" });
+//         lista_pomocnicza.push({ Title: "Sierotka Marysia2", Description: "Whatever Happens" });
+//         $scope.lists.push(lista_pomocnicza);
+//        // $scope.lists.i.push({ Title: "Sierotka Marysia", Description: "Whatever" });
+//     }*/
+//    $http({
+//        method: "GET",
+//        url: "/IterationPlan/GetNumber",
+//        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+//    }).then(function mySucces(response) {
+//        $scope.message = response.data;
+//        $scope.iterations = response.data;
+//        for (var i = 0; i < $scope.iterations.length; ++i) {
+//            var id = $scope.iterations[i].id;
+//            $http({
+//                method: "POST",
+//                url: "/IterationPlan/GetIteration",
+//                data: $.param({ "id": $scope.iterations[i].id }),
+//                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
 
-            }).then(function mySucces(response) {
-                var list = [];
-                list.push(response.data);
-                $scope.lists.push(response.data);
-                $scope.message = response.data;
-            }, function myError(response) {
-                $scope.message = "Error";
-            })
-        }
-    }, function myError(response) {
-        $scope.message = "Error";
-    })
+//            }).then(function mySucces(response) {
+//                var list = [];
+//                list.push(response.data);
+//                $scope.lists.push(response.data);
+//                $scope.message = response.data;
+//            }, function myError(response) {
+//                $scope.message = "Error";
+//            })
+//        }
+//    }, function myError(response) {
+//        $scope.message = "Error";
+//    })
 
-    $scope.addIteration = function () {
-        $http({
-            method: "POST",
-            url: "/IterationPlan/AddingIteration",
-            data: $.param({ "name": $scope.iterationName, "description": $scope.iterationDescription, "startDate": $scope.startDate, "endDate": $scope.endDate }),
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+//    $scope.addIteration = function () {
+//        $http({
+//            method: "POST",
+//            url: "/IterationPlan/AddingIteration",
+//            data: $.param({ "name": $scope.iterationName, "description": $scope.iterationDescription, "startDate": $scope.startDate, "endDate": $scope.endDate }),
+//            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
 
-        }).then(function mySucces(response) {
-            $scope.message = "Result: " + response.data;
-            if (response.data == "Success")
-                window.location.href = "/IterationPlan/Index";
-        }, function myError(response) {
-            $scope.message = "Error in adding iteration";
-        })
-
-
-    }
-
-    //var item = { "Title": "Sierotka Marysia", "Description": "Whatever" };
-    //$scope.lists.push({Title: "Sierotka Marysia", Description: "Whatever"});
-    /*
-    function loadData() {
-        iterations.push = { "Name": "Backlog", "StartDate": "dhefuieff", "EndDate": "jeoefof;rg" };
-        var item = { "Title": "Sierotka Marysia", "Description": "Whatever" };
-        lists.push = item;
-
-        $http({
-            method: "GET",
-            url: "/IterationPlan/GetNumber",
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
-        }).then(function mySucces(response) {
-            iterations.push = response.data;
-            getIterationsData();
-        }, function myError(response) {
-            $scope.message = "Error";
-        })
-
-        $scope.getIterationsData = function () {
-            for (var i = 0; i < iterations.length() ; ++i) {
-                $http({
-                    method: "POST",
-                    url: "/IterationPlan/GetIteration",
-                    data: $.param({ "name": iterations.Name }),
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
-
-                }).then(function mySucces(response) {
-                    lists.push = response.data;
-                }, function myError(response) {
-                    $scope.message = "Error";
-                })
-            }
+//        }).then(function mySucces(response) {
+//            $scope.message = "Result: " + response.data;
+//            if (response.data == "Success")
+//                window.location.href = "/IterationPlan/Index";
+//        }, function myError(response) {
+//            $scope.message = "Error in adding iteration";
+//        })
 
 
-        }
-    }
+//    }
 
-    loadData();*/
+//    //var item = { "Title": "Sierotka Marysia", "Description": "Whatever" };
+//    //$scope.lists.push({Title: "Sierotka Marysia", Description: "Whatever"});
+//    /*
+//    function loadData() {
+//        iterations.push = { "Name": "Backlog", "StartDate": "dhefuieff", "EndDate": "jeoefof;rg" };
+//        var item = { "Title": "Sierotka Marysia", "Description": "Whatever" };
+//        lists.push = item;
+
+//        $http({
+//            method: "GET",
+//            url: "/IterationPlan/GetNumber",
+//            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+//        }).then(function mySucces(response) {
+//            iterations.push = response.data;
+//            getIterationsData();
+//        }, function myError(response) {
+//            $scope.message = "Error";
+//        })
+
+//        $scope.getIterationsData = function () {
+//            for (var i = 0; i < iterations.length() ; ++i) {
+//                $http({
+//                    method: "POST",
+//                    url: "/IterationPlan/GetIteration",
+//                    data: $.param({ "name": iterations.Name }),
+//                    headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+
+//                }).then(function mySucces(response) {
+//                    lists.push = response.data;
+//                }, function myError(response) {
+//                    $scope.message = "Error";
+//                })
+//            }
 
 
-}]);
+//        }
+//    }
+
+//    loadData();*/
+
+
+//}]);
