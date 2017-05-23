cf73c823d387581550f6337b45cf370e5848ef46
Anna Kocztorz <annakoc476@student.polsl.pl>
2017-05-23 16:53:09 +0200

    Dodanie możliwości dodawwania tasków z poziomu iteracji

--- SeeNowProcess/Scripts/iterationPlan.js
+++ SeeNowProcess/Scripts/iterationPlan.js
@@ -126,3 +126,54 @@ iterationApp.controller("iterationCtrl", ['$scope', '$http', function ($scope, $
 
         }
 }]);
+
+iterationApp.controller("taskCtrl", function ($scope,$http) {
+
+    /* Roksana:
+    1. Nie dodaję na razie "Choose an option" które dodała Ania, zostaje puste pole które po wyborze opcji
+    znika z listy wyboru. Nie wiem na razie czy to dobry pomysł, zależy czy to będzie obowiązkowe pole (?)
+    2. Chyba niepotrzebnie dodałam to jako obiekty, ale proszę o weryfikację */
+    $scope.status_options =
+        [
+            { value: "open", description: "Open" },
+            { value: "suspended", description: "Suspended" },
+            { value: "closed", description: "Closed" }
+        ];
+    $scope.importance_options =
+        [
+             { value: "none", description: "None" },
+             { value: "trivial", description: "Trivial" },
+             { value: "regular", description: "Regular" },
+             { value: "important", description: "Important" },
+             { value: "critical", description: "Critical" }
+        ];
+    $scope.parent_options =
+        [
+             { value: "s", description: "Small (S)" },
+             { value: "m", description: "Medium (M)" },
+             { value: "l", description: "Large (L)" },
+             { value: "xl", description: "Extra large (XL)" },
+        ];
+    $scope.addTask = function () {
+        $scope.tasks.push({
+            title: $scope.title, description: $scope.description, status: $scope.status.value,
+            importance: $scope.importance.value, estimated_time: $scope.estimated_time, parent: $scope.parent.value
+        });
+        $http({
+            method: "POST",
+            url: "/Add/IndexAdd",
+            data: $.param({
+                'title': $scope.title, 'description': $scope.description,
+                'status': $scope.status.value, 'importance': $scope.importance.value,
+                'estimated_time': $scope.estimated_time, 'parent': $scope.parent.value
+            }),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+        }).then(function success(response) {
+            $scope.message = "Did it!";
+        },
+        function failure(response)
+        {
+            $scope.message = "Fail...";
+        });
+    }
+});
