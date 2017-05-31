d42b9159eecfa3825ad03a9491079254bc5a228a
Roksana Paluch <rokspal895@student.polsl.pl>
2017-06-01 01:01:39 +0200

    Backlog - poczatki istnienia

--- SeeNowProcess/Controllers/BacklogController.cs
+++ SeeNowProcess/Controllers/BacklogController.cs
@@ -4,14 +4,63 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Projekt_programistyczny_pierwsze_kroki.Controllers
+namespace SeeNowProcess.Controllers
 {
-    public class BacklogController : Controller
+    public class BacklogController : DIContextBaseController
     {
         // GET: Backlog
-        public ActionResult Index()
+        public ActionResult BacklogIndex()
         {
             return View();
+        }
+
+        public ActionResult GetUserStories()
+        {
+            using (db)
+            {
+                var resultJ = db.UserStories.Select(a => new
+                {
+                    UserSID = a.UserStoryID,
+                    Title = a.Title
+                });
+                return new JsonResult { Data = resultJ.ToList(), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GetUserStory(int id)
+        {
+            using (db)
+            {
+                var userstory = db.UserStories.Where(u => u.UserStoryID == id)
+                        .Select(p => new
+                        {
+                            Description = p.Description,
+                            Size = p.Size,
+                            Unit = p.Unit,
+                            Notes = p.Notes,
+                            Criteria = p.Criteria,
+                            Owner = p.Owner.Name,
+                            Project = p.Project.Name
+                        });
+                return Json(userstory.ToList(), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GetUSTasks(int id)
+        {
+            using (db)
+            {
+                var tasks = db.Problems.Where(u => u.Story.UserStoryID == id)
+                        .Select(p => new
+                        {
+                            Title = p.Title,
+                            Description = p.Description
+                            //wysylac jeszcze inne informacje o tasku potem, np. order
+                        });
+                return Json(tasks.ToList(), JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
---
+++ SeeNowProcess/Scripts/angularBacklog.js
@@ -0,0 +1,50 @@
+﻿var backlogAngular = angular.module("backlog", []);
+backlogAngular.controller("backlogCtrl", ['$scope', '$http', function ($scope, $http) {
+    $scope.userstory = "Select user story to show content";
+    $scope.currentUS;
+    $scope.tasksUS;
+    $scope.info;
+
+    $http({
+        method: "GET",
+        url: "/Backlog/GetUserStories",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+    }).then(function mySucces(response) {
+        $scope.userstories = response.data;
+    }, function myError(response) {    
+    })
+
+    $scope.showUserStory = function (id, userstory) {
+        $scope.userstory = userstory;
+        
+        $http({
+            method: "POST",
+            url: "/Backlog/GetUserStory",
+            data: $.param({ "id": id }),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+        }).then(function success(result) {
+            $scope.currentUS = result.data[0];
+        }).catch(function fail(result) {
+            $scope.problems = ":(";
+        })
+        $http({
+            method: "POST",
+            url: "/Backlog/GetUSTasks",
+            data: $.param({ "id": id }),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+        }).then(function success(result) {
+            $scope.tasksUS = result.data;
+        }).catch(function fail(result) {
+            $scope.problems = ":(";
+        })
+        $scope.info = {
+            description: 'Description',
+            notes: 'Notes',
+            criteria: 'Criteria',
+            owner: 'Owner: ',
+            project: 'Project: ',
+            size: 'Size: '
+        };
+        
+    }
+}]);
