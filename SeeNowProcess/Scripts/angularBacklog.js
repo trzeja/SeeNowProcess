426ae85ee0182f3e7459cd56118d58b0a40f3c8b
Roksana Paluch <rokspal895@student.polsl.pl>
2017-06-12 21:29:04 +0200

    Chcialam naprawic, ale nie wyszlo tylko jest inna metoda to samo a moze jeszcze gorzej :(

--- SeeNowProcess/Scripts/angularBacklog.js
+++ SeeNowProcess/Scripts/angularBacklog.js
@@ -1,25 +1,53 @@
-﻿var backlogAngular = angular.module("backlog", []);
-backlogAngular.controller("backlogCtrl", ['$scope', '$http', function ($scope, $http) {
-    $scope.userstory = "Select user story to show content";
+﻿'use strict';
+var backlogAngular = angular.module('backlog', ['ngRoute']);
+
+//backlogAngular.config(['$routeProvider', 'srvFactory', function ($routeProvider, srvFactory) {
+//    $routeProvider.when('/BacklogIndex', {
+//        templateUrl: 'Backlog/BacklogIndex.cshtml',
+//        controller: 'backlogCtrl',
+//        resolve: {
+//            httpCurrentProject: function (srvFactory) {
+//                return srvFactory.getProject();
+//            }
+//        }
+//    })
+//}]);
+
+backlogAngular.factory('srvFactory', ['$http', function ($http) {
+    var projectdata = {
+        getProject: function () {
+            var promise = $http({
+                method: 'GET',
+                url: '/Backlog/GetCurrentProject',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+            }).success(function (data, status, header, config) { return data; })
+            return promise;
+        }
+    }
+    return projectdata;
+}])
+
+backlogAngular.controller('backlogCtrl', ['$scope', '$http', 'srvFactory',function ($scope, $http, srvFactory) {
+    $scope.userstory = 'Select user story to show content';
     $scope.currentUS;
     $scope.tasksUS;
     $scope.info;
 
 
     $scope.show = 0;
-    $scope.currentProject;
+    $scope.currentProject = srvFactory.getProject().data;
     $scope.projects;
 
     $scope.$watch('currentProject', function () {
         if ($scope.show != 0) {
             $http({
-                method: "POST",
-                url: "/Backlog/ChangeCurrentProject",
+                method: 'POST',
+                url: '/Backlog/ChangeCurrentProject',
                 data: $.param({ id: $scope.currentProject }),
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
             }).then(function mySucces(response) {
-                if (response.data == "Change") {
-                    window.location.href = "/Backlog/BacklogIndex";
+                if (response.data == 'Change') {
+                    window.location.href = '/Backlog/BacklogIndex';
                 }
             }, function myError(response) {
             })
@@ -31,26 +59,26 @@ backlogAngular.controller("backlogCtrl", ['$scope', '$http', function ($scope, $
     });
 
     $http({
-        method: "GET",
-        url: "/Backlog/GetProjects",
+        method: 'GET',
+        url: '/Backlog/GetProjects',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
     }).then(function mySucces(response) {
         $scope.projects = response.data;
     }, function myError(response) {
     })
 
-    $http({
-        method: "GET",
-        url: "/Backlog/GetCurrentProject",
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
-    }).then(function mySucces(response) {
-        $scope.currentProject = response.data;
-    }, function myError(response) {
-    })
+    //$http({
+    //    method: "GET",
+    //    url: "/Backlog/GetCurrentProject",
+    //    headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+    //}).then(function mySucces(response) {
+    //    $scope.currentProject = response.data;
+    //}, function myError(response) {
+    //})
 
     $http({
-        method: "GET",
-        url: "/Backlog/GetUserStories",
+        method: 'GET',
+        url: '/Backlog/GetUserStories',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
     }).then(function mySucces(response) {
         $scope.userstories = response.data;
@@ -61,24 +89,24 @@ backlogAngular.controller("backlogCtrl", ['$scope', '$http', function ($scope, $
         $scope.userstory = userstory;
         
         $http({
-            method: "POST",
-            url: "/Backlog/GetUserStory",
-            data: $.param({ "id": id }),
+            method: 'POST',
+            url: '/Backlog/GetUserStory',
+            data: $.param({ 'id': id }),
             headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
         }).then(function success(result) {
             $scope.currentUS = result.data[0];
         }).catch(function fail(result) {
-            $scope.problems = ":(";
+            $scope.problems = ':(';
         })
         $http({
-            method: "POST",
-            url: "/Backlog/GetUSTasks",
-            data: $.param({ "id": id }),
+            method: 'POST',
+            url: '/Backlog/GetUSTasks',
+            data: $.param({ 'id': id }),
             headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
         }).then(function success(result) {
             $scope.tasksUS = result.data;
         }).catch(function fail(result) {
-            $scope.problems = ":(";
+            $scope.problems = ':(';
         })
         $scope.info = {
             description: 'Description',
