6b639ebd593bbf2a9d51d557988b8f4c088d84cd
Anna Kocztorz <annakoc476@student.polsl.pl>
2017-09-04 20:22:25 +0200

    Dodanie wyboru roli przy rejestracji do systemu.

--- SeeNowProcess/Scripts/angularRegister.js
+++ SeeNowProcess/Scripts/angularRegister.js
@@ -1,6 +1,19 @@
 ﻿var registerApp = angular.module("registerForm", []);
 registerApp.controller("registerCtrl",  [ '$scope', '$http', function ($scope, $http) {
     $scope.message = '';
+
+    $scope.roles_options =
+    [
+         { value: "admin", description: "Admin" },
+         { value: "headMaster", description: "Head Master" },
+         { value: "seniorDev", description: "Senior Developer" },
+         { value: "juniorDev", description: "Junior Developer" },
+         { value: "intern", description: "Intern" },
+         { value: "tester", description: "Tester" },
+         { value: "client", description: "Client" },
+
+
+    ];
     $scope.registerUser = function () {
         if ($scope.password !== $scope.confirmPassword) {
             $scope.message = "Passwords don't match!";
@@ -9,7 +22,7 @@ registerApp.controller("registerCtrl",  [ '$scope', '$http', function ($scope, $
             $http({
                 method: "POST",
                 url: "/Account/RegisterAction",
-                data: $.param({ 'password': $scope.password, 'login': $scope.login, 'name': $scope.name, 'email': $scope.email, 'phone': $scope.phone }),
+                data: $.param({ 'password': $scope.password, 'login': $scope.login, 'name': $scope.name, 'email': $scope.email, 'phone': $scope.phone, 'role': $scope.role.value }),
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
 
             }).then(function mySucces(response) {
