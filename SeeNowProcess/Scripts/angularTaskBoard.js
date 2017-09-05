fcc4f7c3dff9c884bdf95864119189820c03c523
Anna Kocztorz <annakoc476@student.polsl.pl>
2017-09-05 21:19:13 +0200

    Dodanie wyświetlania nazwy projektu przy userStory w taskboardzie dla wszystkich projektów.

--- SeeNowProcess/Scripts/angularTaskBoard.js
+++ SeeNowProcess/Scripts/angularTaskBoard.js
@@ -1,4 +1,9 @@
 ﻿var taskBoardAngular = angular.module("taskBoardPart", ['dndLists']);
+taskBoardAngular.filter('trustAsHtml', ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);
 taskBoardAngular.controller("taskBoardCtrl", ['$scope', '$http', function ($scope, $http) {
     $scope.message = "DZIALAAA";
     $scope.show = 0;
@@ -35,64 +40,72 @@ taskBoardAngular.controller("taskBoardCtrl", ['$scope', '$http', function ($scop
         headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
     }).then(function mySucces(response) {
         $scope.currentProject = response.data;
-    }, function myError(response) {
-    })
-
-    $http({
-        method: "GET",
-        url: "/TaskBoard/GetBoxes",
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
-    }).then(function success(result) {
-        //pobrac tu trzeba tez do ktorego nalezy 
-        //$scope.models.lists.A = result.data;
-        $scope.boxes = result.data;
         $http({
             method: "GET",
-            url: "/TaskBoard/GetUserStories",
+            url: "/TaskBoard/GetBoxes",
             headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
         }).then(function success(result) {
-            //pobrac tu trzeba tez do ktorego nalezy -> nie wiem o co mi chodzilo...
+            //pobrac tu trzeba tez do ktorego nalezy 
             //$scope.models.lists.A = result.data;
-            $scope.userstories = result.data;
-
+            $scope.boxes = result.data;
             $http({
                 method: "GET",
-                url: "/TaskBoard/GetProblems",
+                url: "/TaskBoard/GetUserStories",
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
             }).then(function success(result) {
-                $scope.problems = result.data;
-                $scope.list = [];
-                //tutaj rozdzial na listy
-                for (var i = 0; i < $scope.userstories.length; i++) {
-                    var userstory = { id: $scope.userstories[i].UserSID, title: $scope.userstories[i].Title, boxes: [] };
-                    $scope.list.push(userstory);
-                    for (var j = 0; j < $scope.boxes.length; j++) {
-                        var box = { order: $scope.boxes[j].BoxOrder, name: $scope.boxes[j].Name, tasks: [] };
-                        $scope.list[i].boxes.push(box);
+                //pobrac tu trzeba tez do ktorego nalezy -> nie wiem o co mi chodzilo...
+                //$scope.models.lists.A = result.data;
+                $scope.userstories = result.data;
+
+                $http({
+                    method: "GET",
+                    url: "/TaskBoard/GetProblems",
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+                }).then(function success(result) {
+                    $scope.problems = result.data;
+                    $scope.list = [];
+                    //tutaj rozdzial na listy
+                    for (var i = 0; i < $scope.userstories.length; i++) {
+                        var userstory;
+                        if ($scope.currentProject == 0) {
+                            userstory = { id: $scope.userstories[i].UserSID, title: $scope.userstories[i].Title + "<br/><br/>" + $scope.userstories[i].ProjectName, boxes: [] };
+                            $scope.list.push(userstory);
+                        }
+                        else {
+                            userstory = { id: $scope.userstories[i].UserSID, title: $scope.userstories[i].Title, boxes: [] };
+                            $scope.list.push(userstory);
+                        }
+                        for (var j = 0; j < $scope.boxes.length; j++) {
+                            var box = { order: $scope.boxes[j].BoxOrder, name: $scope.boxes[j].Name, tasks: [] };
+                            $scope.list[i].boxes.push(box);
+                        }
                     }
-                }
-                for (var i = 0; i < $scope.problems.length; i++) {
-                    for (var j = 0; j < $scope.list.length; j++) {
-                        if ($scope.problems[i].UserSID == $scope.list[j].id) //czy jest w tym userstory
-                        {
-                            for (var k = 0; k < $scope.list[j].boxes.length; k++) {
-                                if ($scope.problems[i].BoxOrder == $scope.list[j].boxes[k].order) //czy w tym boxie
-                                {
-                                    $scope.list[j].boxes[k].tasks.push($scope.problems[i]);
+                    for (var i = 0; i < $scope.problems.length; i++) {
+                        for (var j = 0; j < $scope.list.length; j++) {
+                            if ($scope.problems[i].UserSID == $scope.list[j].id) //czy jest w tym userstory
+                            {
+                                for (var k = 0; k < $scope.list[j].boxes.length; k++) {
+                                    if ($scope.problems[i].BoxOrder == $scope.list[j].boxes[k].order) //czy w tym boxie
+                                    {
+                                        $scope.list[j].boxes[k].tasks.push($scope.problems[i]);
+                                    }
                                 }
                             }
                         }
                     }
-                }
+                }).catch(function fail(result) {
+                    $scope.problems = ":(";
+                })
             }).catch(function fail(result) {
-                $scope.problems = ":(";
+                $scope.userstories = ":(";
             })
         }).catch(function fail(result) {
-            $scope.userstories = ":(";
+            $scope.boxes = ":(";
         })
-    }).catch(function fail(result) {
-        $scope.boxes = ":(";
+    }, function myError(response) {
     })
+
+    
 
     $scope.dropCallback = function (item, userstory, box) { //userstory->id, box->order
         //w item mamy jaki był poprzednio - w number jaki ma byc nowy
