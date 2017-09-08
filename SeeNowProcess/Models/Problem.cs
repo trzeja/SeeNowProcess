40981008d84f0ba1b87511bb0926aa3d7afb6fce
iko <trzeja.mikolaj@gmail.com>
2017-09-08 19:27:46 +0200

    wincyjRestrykcyjProblem

--- SeeNowProcess/Models/Problem.cs
+++ SeeNowProcess/Models/Problem.cs
@@ -25,21 +25,27 @@ namespace SeeNowProcess.Models
         public string Description { get; set; }
         [Required]
         public Importance Importance { get; set; }
-        public int? Progress { get; set; }
+        [Required]
+        public int Progress { get; set; }
 
         [Display(Name = "Creation Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreationDate { get; set; }
+        [Required]
         public virtual Box Box { get; set; }
+        [Required]
         public virtual Iteration Iteration { get; set; }
         
         [Display(Name = "Estimated Time")]
+        [Required]
         public TimeSpan EstimatedTime { get; set; } //new TimeSpan(2, 14, 18); "02:14:18"
 
         [Display(Name = "Final Time")]
         public TimeSpan? FinalTime { get; set; }
+        [Required]
         public virtual UserStory Story { get; set; }
+        [Required]
         public virtual ICollection<User> AssignedUsers { get; set; }
         
     } 
